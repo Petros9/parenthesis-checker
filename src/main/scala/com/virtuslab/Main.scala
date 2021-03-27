1937e952e87a3abca4a02eeaac067bb426252eb5
Petros9 <58508596+Petros9@users.noreply.github.com>
2021-03-27 15:52:07 +0100

    basic solution using stack

--- src/main/scala/com/virtuslab/Main.scala
+++ src/main/scala/com/virtuslab/Main.scala
@@ -2,6 +2,9 @@ package com.virtuslab
 
 import java.nio.file.Paths
 import java.nio.file.Files
+import java.util
+
+import scala.annotation.tailrec
 import scala.jdk.CollectionConverters._
 
 /**
@@ -9,13 +12,13 @@ import scala.jdk.CollectionConverters._
   */
 object Main {
 
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     val code = "class A{ method(() }"
     println("### Input code")
     println(code)
     println()
     println("### Does it have correct parenthesis?")
-    if (areCorrectParenthesis(code)) println("Yes!")
+    if (areCorrectParenthesis(code, 0, stack = new util.Stack[Int])) println("Yes!")
     else println("No!")
   }
 
@@ -34,9 +37,35 @@ object Main {
     * TODO We should also return the position of the first parenthesis that is not closed properly.
     * This will require changing the signature of the method, there are multiple possible approaches here.
     *
-    * @param code scala code to evaluate
+    * stack scala code to evaluate
     * @return Boolean answer to the the posed question.
     */
-  def areCorrectParenthesis(code: String): Boolean = false
+
+  def addToStack(stack: util.Stack[Int], number: Int): util.Stack[Int] = {
+    stack.push(number)
+    stack
+  }
+
+  def takeFromStack(stack: util.Stack[Int]): util.Stack[Int] = {
+    stack.pop()
+    stack
+  }
+
+  @tailrec
+  def areCorrectParenthesis(code: String, currentPosition: Int, stack: util.Stack[Int]): Boolean = {
+    if (currentPosition.equals(code.length)) {
+        stack.empty()
+
+    } else if (code.charAt(currentPosition).equals('(')) {
+      Main.areCorrectParenthesis(code, currentPosition + 1, addToStack(stack, 1))
+
+    } else if (code.charAt(currentPosition).equals(')')) {
+      if (stack.empty()) Main.areCorrectParenthesis(code, code.length, addToStack(stack, 1))
+      else Main.areCorrectParenthesis(code, currentPosition + 1, takeFromStack(stack))
+
+    } else {
+      Main.areCorrectParenthesis(code, currentPosition+1, stack)
+    }
+  }
 
 }
